2294ac33ad60b49c1aa7490674d11521ab027033
Semir Patel <spatel@yelp.com>
2014-02-05 12:33:28 -0800

    Add PyHamcrest as a unit test dependency

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ requirements = [
     "pyyaml >= 3.09",
     "tornado >= 1.1, <2.0",
     "testify == 0.3.10",
+    "PyHamcrest >= 1.8",
 ]
 
 # python-rrdtool doesn't install cleanly out of the box on OS X
