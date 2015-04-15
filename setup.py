0a840a38d86fc780805cccf8ab9eb2c1b9683110
Ya-Lin Huang <yalinh@yelp.com>
2015-04-14 18:11:33 -0700

    Bump the version due to pulling in #91

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ if 'sdist' in sys.argv or 'bdist_wheel' in sys.argv:
 
 setup(
     name='firefly',
-    version='1.1.10',
+    version='1.1.11',
     provides=['firefly'],
     author='Yelp',
     description='A multi-datacenter graphing tool',
